84ab3c29ac70d1ede063b71b3e71c4749b9edc21
amilkar checa <cheko_999_@hotmail.com>
2019-04-05 13:13:11 -0400

    preg 1 corregido

--- pregunta1.js
+++ pregunta1.js
@@ -1,4 +1,4 @@
-function sumaMatriz(){
+function sumaMatriz(n){
   var matriz=[[0,1,1,2],[0,5,0,0],[2,1,1,3]];
   var suma=0;
   for(var i=0; i<3; i++){
@@ -6,9 +6,13 @@ function sumaMatriz(){
       if(matriz[i][j] !=0){
         if(i==0)
         suma=suma+matriz[i][j];
+        else {
+          if(matriz[i-1][j]!=0)
+          suma =suma + matriz[i][j]
+        }
       }
     }
   }
-
+return suma;
 }
-console.log sumaMatriz
+console.log (sumaMatriz([[0,1,1,2],[0,5,0,0],[2,1,1,3]]));
